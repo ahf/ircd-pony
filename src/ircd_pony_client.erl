b251244a0e4cef820c46756b69ca2393de04b76f
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2012-07-01 00:38:53 +0200

    Alter the pony client to a gen_server.

--- src/ircd_pony_client.erl
+++ src/ircd_pony_client.erl
@@ -2,9 +2,75 @@
 
 -export([start_link/4]).
 
-start_link(ListenerPid, Socket, Transport, _Opts) ->
+-behaviour(gen_server).
+
+%% API
+-export([start_link/4]).
+
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+         terminate/2, code_change/3]).
+
+-define(SERVER, ?MODULE). 
+
+-record(state, { transport, socket, listener }).
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+%% @doc
+%% Starts the server
+%% @end
+%%--------------------------------------------------------------------
+start_link(ListenerPid, Socket, Transport, Opts) ->
+    gen_server:start_link({local, ?SERVER}, ?MODULE, [ListenerPid,
+                                                      Socket,
+                                                      Transport,
+                                                      Opts], []).    
+
+%%%===================================================================
+%%% gen_server callbacks
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Initializes the server
+%%
+%% @spec init(Args) -> {ok, State} |
+%%                     {ok, State, Timeout} |
+%%                     ignore |
+%%                     {stop, Reason}
+%% @end
+%%--------------------------------------------------------------------
+init([ListenerPid, Socket, Transport, _Opts]) ->
     ranch:accept_ack(ListenerPid),
     Transport:close(Socket),
+    {ok, #state{ listener = ListenerPid,
+                 transport = Transport,
+                 socket = Socket }}.
+
+%% @private
+handle_call(_Request, _From, State) ->
+    Reply = ok,
+    {reply, Reply, State}.
+
+%% @private
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+%% @private
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+%% @private
+terminate(_Reason, _State) ->
     ok.
 
+%% @private
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%%%===================================================================
 
