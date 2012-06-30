ac06decf976b3b1c831a0e68ccadbc450f09634c
Alexander Færøy <ahf@0x90.dk>
2012-07-01 00:33:00 +0200

    Convert known commands to atoms in the IRC tokenizer.
    This patch converts known commands into atoms in the IRC protocol
    tokenizer to make it easier for us to match against the commands later
    on.
    
    In case of an unknown command, a tuple with an unknown token and the
    input string is returned. For example: {unknown, "FOO #test"}.

--- src/ircd_pony_protocol.erl
+++ src/ircd_pony_protocol.erl
@@ -1,6 +1,37 @@
 -module(ircd_pony_protocol).
 -export([parse/1]).
 
+command_to_atom(X) when is_binary(X) ->
+    command_to_atom(binary_to_list(X));
+command_to_atom(X) when is_list(X) ->
+    case string:to_lower(X) of
+        "admin" -> admin;
+        "away" -> away;
+        "invite" -> invite;
+        "join" -> join;
+        "kick" -> kick;
+        "kill" -> kill;
+        "list" -> list;
+        "mode" -> mode;
+        "motd" -> motd;
+        "names" -> names;
+        "nick" -> nick;
+        "notice" -> notice;
+        "oper" -> oper;
+        "part" -> part;
+        "pass" -> pass;
+        "ping" -> ping;
+        "privmsg" -> privmsg;
+        "quit" -> quit;
+        "topic" -> topic;
+        "user" -> user;
+        "version" -> version;
+        "who" -> who;
+        "whois" -> whois;
+        "whowas" -> whowas;
+        _ -> {unknown, X}
+    end.
+
 parse_argument(<<$:, X/binary>>) ->
     [X];
 parse_argument(<<X/binary>>) ->
@@ -11,7 +42,7 @@ parse_argument(<<X/binary>>) ->
 
 parse_command(Prefix, X) ->
     case binary:split(X, <<" ">>) of
-        [Command, Rest] -> {ok, Prefix, Command, parse_argument(Rest)};
+        [Command, Rest] -> {ok, Prefix, command_to_atom(Command), parse_argument(Rest)};
         _ -> {error, {missing_command, Prefix, X}}
     end.
 
