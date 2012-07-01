3ca266c22838811468ff7d5881dfc1eab1682f55
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2012-07-01 12:17:17 +0200

    Introduce lager, update client code a bit.

--- src/ircd_pony_client.erl
+++ src/ircd_pony_client.erl
@@ -13,7 +13,7 @@
 
 -define(SERVER, ?MODULE). 
 
--record(state, { transport, socket, listener }).
+-record(state, { transport, socket, listener, synchronized }).
 
 %%%===================================================================
 %%% API
@@ -34,12 +34,15 @@ start_link(ListenerPid, Socket, Transport, Opts) ->
 %% @private
 init([ListenerPid, Socket, Transport, _Opts]) ->
     %% This should be called when we know the handshake went well
-    ranch:accept_ack(ListenerPid),
+%%    ranch:accept_ack(ListenerPid),
     %% Quickly Close the socket
-    Transport:close(Socket),
+    %%    Transport:close(Socket),
+    Transport:setopts(Socket,
+                      [raw, binary]),
     {ok, #state{ listener = ListenerPid,
                  transport = Transport,
-                 socket = Socket }}.
+                 socket = Socket,
+                 synchronized = false }, 0}.
 
 %% @private
 handle_call(_Request, _From, State) ->
@@ -51,7 +54,14 @@ handle_cast(_Msg, State) ->
     {noreply, State}.
 
 %% @private
-handle_info(_Info, State) ->
+handle_info(timeout, #state { synchronized = false,
+                              transport = Transport,
+                              socket = Socket } = State) ->
+    sync(Socket),
+    Transport:setopts(Socket, {active, once}),
+    {noreply, State};
+handle_info(Info, State) ->
+    lager:warning("Unknown message received: ~p", [Info]),
     {noreply, State}.
 
 %% @private
@@ -63,4 +73,8 @@ code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
 
 %%%===================================================================
+
+%% @doc Synchronize the socket
+sync(_Socket) ->
+    todo.
 
